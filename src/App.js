556d2dc422e5626408655cf3eae073ea1914b3ef
Suman Dutta <110901366+suman3262@users.noreply.github.com>
2023-04-11 12:07:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
+import "./App.css";
+
+
+function App() {
+  return (
+     <div>
+       App
+
+      
+
+       <Router>
+        <Routes>
+          <Route />
+        </Routes>
+       </Router>
+     </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChakraProvider,ColorModeScript ,theme} from "@chakra-ui/react";
+import ColorModeSwitcher from "./ColorModeSwitcher";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+  <ColorModeScript/>
+
+  <ChakraProvider theme={theme}>
+  <ColorModeSwitcher/>
+      <App />
+  </ChakraProvider>
+    
+  </React.StrictMode>
+);
